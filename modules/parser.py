66c84fa5d825d0a2dc6f60b81820ff858d14b6a6
sysophost <sysophost>
2020-07-07 18:10:42 +1000

    moved fqdn resolution into a dedicate function, tidied up share permissions stuff

--- modules/parser.py
+++ modules/parser.py
@@ -40,26 +40,15 @@ def parse_services(report_host: ET.Element, use_fqdns: bool) -> list:
     report_items = report_host.findall('ReportItem')
 
     parsed_services = list()
-    fqdn = None
+    if use_fqdns:
+        fqdn = parse_fqdns(report_host)
+    else:
+        fqdn = None
 
-    fqdns = list(filter(lambda x: x.attrib['pluginName'] == 'Host Fully Qualified Domain Name (FQDN) Resolution', report_items))
     detected_services = list(filter(lambda x: x.attrib['pluginName'] == 'Service Detection', report_items))
 
-    if len(fqdns) > 1:
-        print("Found multiple hostnames")
-        # TODO: If multiple host entries are matched, insert both?
-        # Maybe add a way to highlight the fact its multiple hostnames for the same box?
-
-    for item in fqdns:
-        plugin_output = getattr(item.find('plugin_output'), 'text')
-        fqdn = re.search('((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}', plugin_output)[0]
-        logging.debug(f"\tFound FQDN: {fqdn}")
-
     for item in detected_services:
-        if use_fqdns and fqdn:
-            hostname = fqdn
-        else:
-            hostname = report_host.get('name')
+        hostname = fqdn or report_host.get('name')
         port = int(item.get('port'))
         service_name = item.get('svc_name')
         protocol = item.get('protocol')
@@ -98,30 +87,18 @@ def parse_shares(report_host: ET.Element, use_fqdns: bool) -> list:
         list -- List of SMB shares for the given ReportHost
     """
 
-    host_properties = report_host.find('HostProperties')
+    if use_fqdns:
+        fqdn = parse_fqdns(report_host)
+    else:
+        fqdn = None
+
     report_items = report_host.findall('ReportItem')
-
     parsed_shares = list()
-    fqdn = None
 
-    fqdns = list(filter(lambda x: x.attrib['pluginName'] == 'Host Fully Qualified Domain Name (FQDN) Resolution', report_items))
     detected_shares = list(filter(lambda x: x.attrib['pluginName'] == 'Microsoft Windows SMB Shares Enumeration', report_items))
 
-    if len(fqdns) > 1:
-        print("Found multiple hostnames")
-        # TODO: If multiple host entries are matched, insert both?
-        # Maybe add a way to highlight the fact its multiple hostnames for the same box?
-
-    for item in fqdns:
-        plugin_output = getattr(item.find('plugin_output'), 'text')
-        fqdn = re.search('((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}', plugin_output)[0]
-        logging.debug(f"\tFound FQDN: {fqdn}")
-
     for item in detected_shares:
-        if use_fqdns and fqdn:
-            hostname = fqdn
-        else:
-            hostname = report_host.get('name')
+        hostname = fqdn or report_host.get('name')
         port = int(item.get('port'))
         service_name = item.get('svc_name')
         protocol = item.get('protocol')
@@ -131,9 +108,69 @@ def parse_shares(report_host: ET.Element, use_fqdns: bool) -> list:
         plugin_output = plugin_output.strip().split('\n')
         for line in plugin_output[3:]:  # skip the first 3 elements as they aren't relevant
             share_name = re.search('^\s{2}-\s(.*)', line)[1]
-            #share_name = 'AAA'
+            # share_name = 'AAA'
             uncpath = f"\\\\{hostname}\\{share_name}"
             share = sh.Share(hostname, port, service_name, protocol, uncpath)
             parsed_shares.append(share)
 
     return parsed_shares
+
+
+def parse_share_permissions(report_host: ET.Element) -> list:
+    """
+    Parse share permissions from the supplied ReportHost element
+
+    Args:
+        report_host (ET.Element): ReportHost element extracted from the current Report element
+
+    Returns:
+        list: List of SMB share permissions for the given ReportHost
+    """
+    host_properties = report_host.find('HostProperties')
+    report_items = report_host.findall('ReportItem')
+
+    parsed_permissions = list()
+
+    all_permissions = list(filter(lambda x: x.attrib['pluginName'] == 'Microsoft Windows SMB Share Permissions Enumeration', report_items))
+
+    for item in all_permissions:
+        plugin_output = getattr(item.find('plugin_output'), 'text')
+        parsed_permissions.append(plugin_output)
+
+    return parsed_permissions
+
+
+def parse_fqdns(report_host: ET.Element) -> str:
+    """
+    Extract FQDN using a variety of plugins for the supplied ReportHost
+
+    Args:
+        report_host (ET.Element): ReportHost element to extract FQDN for
+
+    Returns:
+        str: FQDN or IP address if no FQDN exists
+    """
+    report_items = report_host.findall('ReportItem')
+    host_properties = report_host.find('HostProperties')
+
+    # There are multiple methods of finding the hostname, this one seems to work for HTTPS services
+    resolved_fqdns = list(filter(lambda x: x.attrib['pluginName'] == 'Host Fully Qualified Domain Name (FQDN) Resolution', report_items))
+    if resolved_fqdns:
+        # TODO: handle cases where multiple hostnames exist
+        for item in resolved_fqdns:
+            plugin_output = getattr(item.find('plugin_output'), 'text')
+            fqdn = re.search('((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}', plugin_output)[0]
+
+    else:
+        host_fqdn = getattr(host_properties.find('tag[@name="host-fqdn"]'), 'text', '')
+        if host_fqdn:
+            fqdn = host_fqdn
+        else:
+            hostname = getattr(host_properties.find('tag[@name="netbios-name"]'), 'text', '')
+            domain = getattr(host_properties.find('tag[@name="wmi-domain"]'), 'text', '')
+
+            if hostname and domain:
+                fqdn = f"{hostname}.{domain}"
+            else:
+                fqdn = getattr(host_properties.find('tag[@name="host-ip"]'), 'text')
+    return fqdn
--- vulcan.py
+++ vulcan.py
@@ -7,8 +7,10 @@ from modules import parser
 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('--inputfile', '-if', type=str, required=True, help='Path to input .nessus file')
+PARSER.add_argument('--services', '-sv', action='store_true', help='Extract all services identified by the Service Detection plugin')
 PARSER.add_argument('--urls', '-u', action='store_true', help='Only print things with http:// or https:// URI')
-PARSER.add_argument('--shares', '-s', action='store_true', help='Extract SMB shares')
+PARSER.add_argument('--shares', '-sh', action='store_true', help='Extract SMB shares')
+PARSER.add_argument('--sharepermissions', '-sp', action='store_true', help='Extract SMB share permissions')
 PARSER.add_argument('--fqdns', '-f', action='store_true', help='Include resolved FQDN')
 PARSER.add_argument('--verbose', '-v', action='store_true', help='Enable verbose output')
 
@@ -27,24 +29,27 @@ def main():
         xml_doc = ET.parse(ARGS.inputfile).getroot()
         service_uris = list()
         share_list = list()
+        share_permissions_list = list()
         # iterate through all Report elements within the provided .nessus file
         for report in xml_doc.findall('Report'):
             report_hosts = parser.parse_hosts(report)
 
             report_issues = list()
             for host in report_hosts:
-                logging.debug(f"[i] Collecting services for host: {host.get('name')}")
-                services = parser.parse_services(host, ARGS.fqdns)
-                if services:
-                    tcp_services = list(filter(lambda x: x.protocol == 'tcp', services))
-                    udp_services = list(filter(lambda x: x.protocol == 'udp', services))
-                    logging.debug(f"\tFound {len(tcp_services)} TCP and {len(udp_services)} UDP services")
 
-                    for service in services:
-                        if (ARGS.urls and service.uri in ['http://', 'https://']) or not ARGS.urls:
-                            service_uris.append(service)
-                else:
-                    logging.debug(f"\tNo services found")
+                if ARGS.services or ARGS.urls:
+                    logging.debug(f"[i] Collecting services for host: {host.get('name')}")
+                    services = parser.parse_services(host, ARGS.fqdns)
+                    if services:
+                        tcp_services = list(filter(lambda x: x.protocol == 'tcp', services))
+                        udp_services = list(filter(lambda x: x.protocol == 'udp', services))
+                        logging.debug(f"\tFound {len(tcp_services)} TCP and {len(udp_services)} UDP services")
+
+                        for service in services:
+                            if (ARGS.urls and service.uri in ['http://', 'https://']) or not ARGS.urls:
+                                service_uris.append(service)
+                    else:
+                        logging.debug(f"\tNo services found")
 
                 if ARGS.shares:
                     logging.debug(f"[i] Collecting shares for host: {host.get('name')}")
@@ -53,14 +58,29 @@ def main():
                         logging.debug(f"\tFound {len(shares)} share(s)")
                         for share in shares:
                             share_list.append(share)
+                    else:
+                        logging.debug(f"\tNo shares found")
+
+                if ARGS.sharepermissions:
+                    logging.debug(f"[i] Collecting share permissions for host: {host.get('name')}")
+                    share_permissions = parser.parse_share_permissions(host)
+                    if share_permissions:
+                        logging.debug(f"\tFound permissions for share(s)")
+                        for permission in share_permissions:
+                            share_permissions_list.append(permission)
+                    else:
+                        logging.debug(f"\tNo share permissions found")
 
         service_uris = sorted(set(service_uris), key=lambda x: x.uri)
         for service in service_uris:
-            print((f"{service.uri}{service.hostname}:{service.port}"))
+            print(f"{service.uri}{service.hostname}:{service.port}")
 
         share_list = sorted(set(share_list), key=lambda x: x.uncpath)
         for share in share_list:
-            print((f"{share.uncpath}"))
+            print(f"{share.uncpath}")
+
+        for permission in share_permissions_list:
+            print(f"{permission}")
 
     except Exception as err:
         logging.error(f"[!] {err}")
